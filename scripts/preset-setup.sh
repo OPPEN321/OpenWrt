2b9ee1e59dd7c5f9be1648052edcb7f93c847b8a
OPPEN321 <zj18139624826@gmail.com>
2025-10-29 17:58:41 +0800

    Update preset-setup.sh

--- scripts/preset-setup.sh
+++ scripts/preset-setup.sh
@@ -4,7 +4,7 @@
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认默认主机名
-sed -i 's/OpenWrt/ZeroWrt/' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/ZeroWrt/' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
