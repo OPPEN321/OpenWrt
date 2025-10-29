cd243a878c60621376fe8ce9f3e9e82ee3696740
OPPEN321 <zj18139624826@gmail.com>
2025-10-29 18:11:02 +0800

    Update preset-setup.sh

--- scripts/preset-setup.sh
+++ scripts/preset-setup.sh
@@ -20,7 +20,6 @@ rm -rf feeds/packages/net/{mosdns,msd_lite,open-app-filter,libxcrypt}
 rm -rf feeds/luci/themes/{luci-theme-argon,luci-theme-netgear}
 rm -rf feeds/luci/applications/luci-app-mosdns,luci-app-netdata}
 rm -rf feeds/packages/lang/golang
-rm -rf package/wwan
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
