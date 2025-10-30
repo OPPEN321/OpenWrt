2e4b39ac21154eb46854394c3a10516480c63530
OPPEN321 <zj18139624826@gmail.com>
2025-10-30 10:01:41 +0800

    Update preset-setup.sh

--- scripts/preset-setup.sh
+++ scripts/preset-setup.sh
@@ -2,9 +2,11 @@
 
 # 修改默认IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/luci/bin/config_generate
 
 # 修改默认默认主机名
 sed -i 's/LEDE/ZeroWrt/' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/ZeroWrt/' package/base-files/luci/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
