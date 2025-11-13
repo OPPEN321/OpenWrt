2e4196b6e06c55cb80eff2f2868635b910ac13ac
OPPEN321 <zj18139624826@gmail.com>
2025-11-13 23:38:36 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 临时解决 NSS 下载错误
-sed -i 's|3ec87f221e8905d4b6b8b3d207b7f7c4666c3bc8db7c1f06d4ae2e78f863b8f4|881cbf75efafe380b5adc91bfb1f68add5e29c9274eb950bb1e815c7a3622807|g' feeds/nss_packages/firmware/nss-firmware/Makefile
-
 # 修复 Rust 错误
 sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
 
