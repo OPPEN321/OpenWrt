e9e10c0273397cb6d3c9c8ed3c67c663edf3161f
OPPEN321 <zj18139624826@gmail.com>
2025-11-14 11:25:52 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -136,6 +136,7 @@ git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAp
 git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+sed -i "/^config athena_led/,/^config / s/^\(\s*option value\s*'\).*\('.*\)/\1ZWRT\2/" package/luci-app-athena-led/root/etc/config/athena_led
 git clone --depth=1 https://github.com/QuickWrt/luci-theme-argon package/luci-theme-argon
 
 ./scripts/feeds update -a
