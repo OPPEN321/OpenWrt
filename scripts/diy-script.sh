553b7ea31ebe73982bc8bb97c957991d239896ed
OPPEN321 <zj18139624826@gmail.com>
2025-11-16 10:44:36 +0800

    Update diy-script.sh

--- scripts/diy-script.sh
+++ scripts/diy-script.sh
@@ -23,6 +23,7 @@ sed -i "s/encryption='.*'/encryption='psk2+ccmp'/g" package/network/config/wifi-
 sed -i "s/country='.*'/country='CN'/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
 # OpenWrt 默认的启动横幅文件
+mkdir files/etc
 cp -f $GITHUB_WORKSPACE/files/banner package/base-files/files/etc/banner
 
 # 修复 Rust 错误
